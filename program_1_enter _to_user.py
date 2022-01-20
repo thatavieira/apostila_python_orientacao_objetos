81361aed9b639dc648b4728e7e22eb5cb54e5493
thata <thagurgel0909@gmail.com>
2022-01-20 15:00:40 -0300

    enter_to_user

---
+++ program_1_enter _to_user.py
@@ -0,0 +1,12 @@
+numero = input('enter a number:\n')
+print(numero)
+
+numero=input('enter a number:\n')
+print('the number enter is ' + numero)
+
+name=input('type your name:\n')
+age=input('type your age:\n')
+print('your name is {} and your age is {}'. format(name , age))
+
+print('my name is ' + name + ' my age is ' + age)
+
