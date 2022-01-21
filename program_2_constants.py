79bf2da5af9a81db71f05c6ebb736c4afee78cb8
thata <thagurgel0909@gmail.com>
2022-01-21 12:21:24 -0300

    constants and command_IF_ELSE

---
+++ program_2_constants.py
@@ -0,0 +1,20 @@
+1 == '1'
+print(1=='1')
+
+2 > 1
+print(2 > 1)
+
+2 < 1
+print(2 < 1)
+
+bool(3 > 5)
+print(3 > 5)
+
+x=(1,2,3)
+y=(1,2,3)
+print('x == y')
+print(x==y)
+print(x is y)
+
+
+
--- program_3_commad _IF.py
+++ program_3_commad _IF.py
@@ -1,6 +1,6 @@
 numero=42
 chute=input('digite um numero: ')
-
+chute=int(chute)
 
 if chute == numero:
     print('voce acertou')
